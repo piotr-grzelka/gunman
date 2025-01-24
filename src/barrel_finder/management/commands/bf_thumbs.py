fcf703ad20789ee6813e84719a6eb81a2bac5d9c
Piotr Grzelka <piotr@grzelka.pro>
2025-01-24 20:52:25 +0100

    thumbs, categories

--- src/barrel_finder/management/commands/bf_thumbs.py
+++ src/barrel_finder/management/commands/bf_thumbs.py
@@ -10,17 +10,27 @@ from src.barrel_finder.models import Ad, AdImage
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        for ad in Ad.objects.filter(Q(thumb__isnull=True) | Q(thumb='')).order_by('-created_at').all()[:100]:
+        for ad in Ad.objects.filter(
+                Q(thumb__isnull=True) | Q(thumb=''),
+                adimage__id__isnull=False
+        ).order_by('-created_at').all()[:100]:
+            print(ad.id, ad.name, end=" ")
+
             image = AdImage.objects.filter(ad=ad).first()
+
             if not image:
                 continue
 
+            print("resizing")
             resized = resize_and_crop_image_from_url(image.url)
 
             if resized:
+                print("resized")
                 _, extension = os.path.splitext(image.url)
                 fn = '/ad/' + ad.id.__str__() + extension
                 path = settings.MEDIA_ROOT + fn
                 resized.save(path)
                 ad.thumb = fn
                 ad.save()
+            else:
+                print("failed")
