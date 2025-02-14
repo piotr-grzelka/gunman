5130b667c75fcc6013e50d8ab2b42463071813fb
Piotr Grzelka <piotr@grzelka.pro>
2025-02-14 19:23:47 +0100

    admin

--- src/barrel_finder/admin.py
+++ src/barrel_finder/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from src.barrel_finder.models import Portal, Category, Ad
+
+
+@admin.register(Portal)
+class PortalAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'qty')
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'description')
+    list_display = ('name', 'portal', 'category', 'kind', 'price', 'created_at', 'date')
+    list_filter = ('portal', 'kind', 'category')
