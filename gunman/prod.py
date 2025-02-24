e21705ad852eaad50fb91f2cbbcb85f060608e83
Piotr Grzelka <piotr@grzelka.pro>
2025-02-24 23:44:51 +0100

    flashcards

--- gunman/prod.py
+++ gunman/prod.py
@@ -2,6 +2,6 @@ from .settings import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['barrel-finder.gunman.pl']
+ALLOWED_HOSTS = ['barrel-finder.gunman.pl', 'testy.gunman.pl']
 SECRET_KEY = os.getenv('SECRET_KEY')
 
