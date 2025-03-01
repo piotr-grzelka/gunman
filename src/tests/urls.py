c8e87a0e5a40b2fbaf5bc111d52308ca449d32d9
Piotr Grzelka <piotr@grzelka.pro>
2025-03-02 00:46:41 +0100

    flashcards

--- src/tests/urls.py
+++ src/tests/urls.py
@@ -6,7 +6,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.flashcard_view, name='index'),
+    path('', views.index_view, name='index'),
+    path('card/random', views.random_flashcard_view, name='flash-card-random'),
     path('card/<str:pk>', views.flashcard_view, name='flash-card'),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/tests/views.py
+++ src/tests/views.py
@@ -3,18 +3,44 @@ from django.shortcuts import render, redirect
 from src.tests.models import Question
 
 
-def flashcard_view(request, pk=None):
-    if not pk:
-        pk = Question.objects.filter(flashcard_question__isnull=False).order_by('?').first().id
-        return redirect('flash-card', pk)
+def index_view(request):
+    return render(request, 'tests/index.html')
 
+
+def random_flashcard_view(request):
+    question = Question.objects.filter(flashcard_question__isnull=False).order_by('?').first()
+    return redirect('flash-card', question.id)
+
+
+def flashcard_view(request, pk):
     question = Question.objects.filter(id=pk).get()
-    random_next = Question.objects.filter(flashcard_question__isnull=False).exclude(id=question.id).order_by('?').first().id
 
     count_all = Question.objects.filter(flashcard_question__isnull=False).count()
 
+    prev_card = Question.objects.filter(
+        flashcard_question__isnull=False,
+        flashcard_no__lt=question.flashcard_no
+    ).order_by('flashcard_no').first()
+
+    if not prev_card:
+        prev_card = Question.objects.filter(
+            flashcard_question__isnull=False
+        ).order_by('flashcard_no').last()
+
+    next_card = Question.objects.filter(
+        flashcard_question__isnull=False,
+        flashcard_no__gt=question.flashcard_no
+    ).order_by('flashcard_no').first()
+
+    if not next_card:
+        next_card = Question.objects.filter(
+            flashcard_question__isnull=False
+        ).order_by('flashcard_no').first()
+
+
     return render(request, 'tests/flashcard.html', {
         'question': question,
-        'random_next': random_next,
+        'prev_card': prev_card,
+        'next_card': next_card,
         'count_all': count_all
     })
